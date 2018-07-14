01ac1bf00ff811881b8593dc6855f32282138747
neophenix <neophenix@gmail.com>
2018-07-14 16:52:56 -0400

    host container running / total counts

--- internal/handlers/handler_hosts.go
+++ internal/handlers/handler_hosts.go
@@ -17,6 +17,29 @@ func HostListHandler(w http.ResponseWriter, r *http.Request) {
         log.Printf("Could not get host resource list %s\n", err.Error())
     }
 
+    // host -> container info mapping
+    hostContainerInfo := make(map[string]map[string]int)
+    // Grab container info without state to see installed vs runnings
+    containerInfo, err := lxd.GetContainers("", "", false)
+    if err != nil {
+        log.Printf("Could not get container list %s\n", err.Error())
+    }
+
+    // Check the status of each container and increment the counter, if we haven't
+    // seen this host before make the map we need
+    for _, container := range containerInfo {
+        if hostContainerInfo[container.Host] == nil {
+            hostContainerInfo[container.Host] = make(map[string]int)
+        }
+        hostContainerInfo[container.Host]["total"] += 1
+
+        if container.Container.Status == "Running" {
+            hostContainerInfo[container.Host]["running"] += 1
+        } else {
+            hostContainerInfo[container.Host]["stopped"] += 1
+        }
+    }
+
     tmpl := readTemplate("host_list.tmpl")
 
     var out bytes.Buffer
@@ -24,6 +47,7 @@ func HostListHandler(w http.ResponseWriter, r *http.Request) {
         "Page": "hosts",
         "Conf": Conf,
         "HostResourceMap": hostResourceMap,
+        "HostContainerInfo": hostContainerInfo,
     })
 
     fmt.Fprintf(w, string(out.Bytes()))
