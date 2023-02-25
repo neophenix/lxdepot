aae5e0fe5a9d37136282cb708b2f44717085eb71
Brian Clapper <neophenix@gmail.com>
2023-02-25 00:01:46 -0500

    add circle buffer that I plan to store messages in that are not read by the websocket yet

---
+++ internal/circularbuffer/circularbuffer.go
@@ -0,0 +1,72 @@
+package circularbuffer
+
+// a simple circular buffer to store messages that our web clients can read later, allowing command results to be stored
+// and read later so new page loads can read what they may have requested previously.
+
+import "time"
+
+// BUFLEN is the length of our buffer, not sure what a good value is here, doesn't need to hold everything but does need
+// to be enough to maintain context on what users are seeing when they read it
+const BUFLEN = 20
+
+// RECENTACCESS is the number of seconds that we compare againt the lastAccess time of a buffer to determine if it was
+// "recent" or not
+const RECENTACCESS = 86400
+
+type CircularBuffer struct {
+	head       int8 // "write" pointer
+	tail       int8 // "read" pointer
+	buffer     [BUFLEN]string
+	lastAccess time.Time
+}
+
+// Enqueue adds a string to our buffer and moves the head foward.  If the new head pointer is going to equal our tail
+// then we are overwriting unread messages and need to push the tail ahead to maintain our circle
+func (c *CircularBuffer) Enqueue(msg string) {
+	// record that we have accessed the buffer
+	c.lastAccess = time.Now()
+	// put our message in
+	c.buffer[c.head] = msg
+	// get our new head pointer
+	newHead := (c.head + 1) % BUFLEN
+	if newHead == c.tail {
+		// maintain our circle if we have caught up to the tail, technically we could still read this tail value and
+		// move on, but it greatly simplifies things to just move it
+		c.tail = (c.tail + 1) % BUFLEN
+	}
+	// finally move the head forward
+	c.head = newHead
+}
+
+// Dequeue takes the first unread message, moves our tail pointer ahead and returns the message.  We also return an "ok"
+// boolean here so we can distinguish between an actual empty string and no message
+func (c *CircularBuffer) Dequeue() (string, bool) {
+	// record that we have accessed the buffer
+	c.lastAccess = time.Now()
+	// if our head and tail are equal there is nothing in our buffer to return
+	if c.head == c.tail {
+		return "", false
+	}
+
+	msg := c.buffer[c.tail]
+	c.tail = (c.tail + 1) % BUFLEN
+	return msg, true
+}
+
+// HasRecentAccess returns true / false if a buffer has been "recently" accessed.  Check the value of RECENTACCESS for
+// what we consider recent
+func (c *CircularBuffer) HasRecentAccess() bool {
+	// buffers shouldn't be created until they are about to be used, this is internal so that should be fine.  So that
+	// being the case we would make it and then immediately put something in it which would set the lastAccess.  So if
+	// its not set then we assume its very new.  This could lead to memory leaks and we will need to move to a constructor
+	// but for now lets experiment with this.
+	if c.lastAccess.IsZero() {
+		return true
+	}
+	diff := time.Now().Sub(c.lastAccess)
+	// we don't need to go too crazy, just compare to seconds in a day
+	if diff.Seconds() < RECENTACCESS {
+		return true
+	}
+	return false
+}
---
+++ internal/circularbuffer/circularbuffer_test.go
@@ -0,0 +1,76 @@
+package circularbuffer
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+type bufTest struct {
+	op    string // operation to perform (enqueue,dequeue)
+	value string // value or csv list of what to enqueue or values that will come from dequeues
+	ok    bool   // only for dequeue, the ok result
+	head  int8   // expected head value
+	tail  int8   // expected tail value
+}
+
+// test enqueue and dequeue operations
+func TestBuffer(t *testing.T) {
+	buffer := &CircularBuffer{}
+
+	tests := []bufTest{
+		{op: "dequeue", value: "", ok: false, head: 0, tail: 0},
+		{op: "enqueue", value: "a", head: 1, tail: 0},
+		{op: "enqueue", value: "b,c,d,e,f,g,h", head: 8, tail: 0},
+		{op: "dequeue", value: "a", ok: true, head: 8, tail: 1},
+		{op: "dequeue", value: "b", ok: true, head: 8, tail: 2},
+		{op: "enqueue", value: "i,j,k,l,m,n,o,p,q,r,s,t", head: 0, tail: 2},
+		{op: "dequeue", value: "c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s", ok: true, head: 0, tail: 19},
+		{op: "dequeue", value: "t", ok: true, head: 0, tail: 0},
+		{op: "dequeue", value: "", ok: false, head: 0, tail: 0},
+	}
+
+	for tidx, test := range tests {
+		if test.op == "enqueue" {
+			for _, v := range strings.Split(test.value, ",") {
+				buffer.Enqueue(v)
+			}
+		} else if test.op == "dequeue" {
+			for _, v := range strings.Split(test.value, ",") {
+				bufval, ok := buffer.Dequeue()
+
+				if test.ok != ok {
+					t.Errorf("%v: expected ok %v got %v", tidx, test.ok, ok)
+				}
+				if v != bufval {
+					t.Errorf("%v: expected %v got %v", tidx, v, bufval)
+				}
+			}
+		}
+
+		if test.head != buffer.head {
+			t.Errorf("%v: expected head to be %v got %v", tidx, test.head, buffer.head)
+		}
+		if test.tail != buffer.tail {
+			t.Errorf("%v: expected tail to be %v got %v", tidx, test.tail, buffer.tail)
+		}
+	}
+}
+
+// some basic tests for recent access where we will force various time objects into a buffer
+func TestRecentAccess(t *testing.T) {
+	buffer := &CircularBuffer{}
+	if !buffer.HasRecentAccess() {
+		t.Error("expected new buffer HasRecentAccess to be true, but it is false")
+	}
+
+	buffer.lastAccess = time.Now().Add(-100 * time.Second)
+	if !buffer.HasRecentAccess() {
+		t.Error("expected recent buffer HasRecentAccess to be true, but it is false")
+	}
+
+	buffer.lastAccess = time.Now().Add(-86401 * time.Second)
+	if buffer.HasRecentAccess() {
+		t.Error("expected old buffer HasRecentAccess to be false, but it is true")
+	}
+}
